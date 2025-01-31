b5aad149c6f5833d64dd0b97b5e212935f17d9ec
Amir 12 <amirbarfar43@gmail.com>
2025-01-31 22:15:14 +0330

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "build",
+  distDir: "build", // تغییر مسیر خروجی به "build"
+  images: {
+    domains: ['todo.amirbarfar.ir'], // دامنه‌های معتبر برای بارگذاری تصاویر
+  },
 };
 
 export default nextConfig;
