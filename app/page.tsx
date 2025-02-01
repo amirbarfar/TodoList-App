7971c35921cb6f92db04a03f1523d0bdde667759
Amir 12 <amirbarfar43@gmail.com>
2025-02-01 13:28:56 +0330

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import {RetroGrid} from '@/components/ui/retro-grid'; 
 import LoadingPage from '@/components/Loader';
